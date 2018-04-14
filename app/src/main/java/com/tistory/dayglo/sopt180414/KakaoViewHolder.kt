7b7d16b9cf1b47d3e97518d7ebfd2f29abea4ed7
ChanJoo <sksckswn@naver.com>
2018-04-14 15:26:44 +0900

    defined view holder class

--- app/src/main/java/com/tistory/dayglo/sopt180414/KakaoViewHolder.kt
+++ app/src/main/java/com/tistory/dayglo/sopt180414/KakaoViewHolder.kt
@@ -1,7 +1,19 @@
 package com.tistory.dayglo.sopt180414
 
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+
 /**
  * Created by user on 2018-04-14.
+ * View를 정의
+ * kakao_item.xml
  */
-class KakaoViewHolder {
+
+class KakaoViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+    var kakaoProfile : ImageView = itemView!!.findViewById(R.id.item_profile_img)
+    var kakaoName : TextView = itemView!!.findViewById(R.id.item_name_tv)
+    var kakaoDate : TextView = itemView!!.findViewById(R.id.item_date_tv)
+    var kakaoPreview : TextView = itemView!!.findViewById(R.id.item_preview_tv)
 }
--- app/src/main/java/com/tistory/dayglo/sopt180414/MainActivity.kt
+++ app/src/main/java/com/tistory/dayglo/sopt180414/MainActivity.kt
@@ -3,10 +3,35 @@ package com.tistory.dayglo.sopt180414
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+*
+* lateinit var 당장 초기화 안하고 일단 넘어갈 경우
+*/
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var kakaoItems : ArrayList<KakaoData> // 여기서 초기화를 하는 방법 or lateinit var
+//    var kakaoItems : ArrayList<KakaoData> =  ArrayList()
+
+    lateinit var  kakaoAdapter : KakaoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        kakaoItems = ArrayList()
+
+        // 객체화 된 것들을 넣어야하기 때문에 사전에 DataClass의 데이터들을 객체화해야됨.
+        kakaoItems.add(KakaoData(R.drawable.img01, "이찬주", "채팅 프리뷰임", "오후 3:19"))
+        kakaoItems.add(KakaoData(R.drawable.img02, "유현호", "채팅 프리뷰임!", "오후 4:19"))
+        kakaoItems.add(KakaoData(R.drawable.img03, "박재영", "채팅 프리뷰임@", "오후 5:19"))
+        kakaoItems.add(KakaoData(R.drawable.img04, "김이현", "채팅 프리뷰임#", "오후 6:19"))
+        kakaoItems.add(KakaoData(R.drawable.img05, "유승렬", "채팅 프리뷰임$", "오후 7:19"))
+        kakaoItems.add(KakaoData(R.drawable.img06, "김우진", "채팅 프리뷰임%", "오후 8:19"))
+        kakaoItems.add(KakaoData(R.drawable.img07, "변진우", "채팅 프리뷰임^", "오후 9:19"))
+        kakaoItems.add(KakaoData(R.drawable.img08, "김민상", "채팅 프리뷰임7", "오후 10:19"))
+        kakaoItems.add(KakaoData(R.drawable.img09, "임태완", "채팅 프리뷰임!", "오후 11:19"))
+        kakaoItems.add(KakaoData(R.drawable.img10, "안도익", "채팅 프리뷰임@", "오후 12:19"))
+
     }
 }
